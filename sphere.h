f62c3b1e2fe2d8625be33f8a957c03899336abc5
Rogger794 <rogger_jrj794@hotmail.com>
2016-09-11 15:20:57 -0500

    nuevo cambio

--- sphere.h
+++ sphere.h
@@ -12,14 +12,16 @@ GLfloat azul[4];//adenina
 GLfloat rojo[4];//citosina
 GLfloat amarillo[4];//timina
 GLfloat verde[4];//guanina
-GLfloat blanco[4];//timina
-GLfloat celeste[4];//guanina
+GLfloat blanco[4];
+GLfloat plomo[4];//puente de hidrogeo
+GLfloat celeste[4];
 GLfloat morado[4];//timina
 }colores;
 
 class TSphere
 {
 private:
+  bool dibujo;
   TSphere * par;
   GLfloat radio;
   GLfloat maxpos;
@@ -33,10 +35,13 @@ public:
   TSphere(GLfloat r,char c);
   ~TSphere();
   void test();
+  void setdibujo(bool v);
   void setcolor(char c);
+  void setpos(GLfloat x, GLfloat y, GLfloat z);
   void link();
   void unlink();
   void render(GLUquadric* g);
+  void render(GLUquadric* g, GLfloat x1, GLfloat y1, GLfloat z1);
   //void modifySpeed(float inc);
   float * getPosv();
 };
