ec8232cd9d923914843eba63e8c4d082dae9bc25
Rogger794 <rogger_jrj794@hotmail.com>
2016-09-18 22:39:14 -0500

    cambio

--- sphere.cpp
+++ sphere.cpp
@@ -238,7 +238,7 @@ void TSphere::render(GLUquadric* g)
     glMaterialfv(GL_FRONT, GL_EMISSION, materialEmission);
     glMaterialf(GL_FRONT, GL_SHININESS, shininess); // parametro de brillantez
     gluSphere (g, radio,20,20);
-   // renderBitmapString(0.0,-0.4,0,GLUT_BITMAP_HELVETICA_18);
+    renderBitmapString(0.0,-0.4,0,GLUT_BITMAP_HELVETICA_18);
     glPopMatrix();
   }
 }
